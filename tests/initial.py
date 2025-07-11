c2ee990aebee8d1f7d93112423c6b5ea14ec80e8
Adam Parsons <adam.a.parsons15@gmail.com>
2025-07-11 09:55:12 +0100

    inital commit

---
+++ tests/initial.py
@@ -0,0 +1,15 @@
+from reachy_sdk import ReachySDK
+import cv2 as cv
+from rplidar import RPLidar
+
+reachy_mobile = ReachySDK(host='172.16.42.113', with_mobile_base=True)
+print(reachy_mobile)
+#reachy_mobile.turn_off("reachy")
+#reachy_mobile.mobile_base.reset_odometry()
+#reachy_mobile.mobile_base.goto(x=0.0, y=-1.0, theta=0.0)
+
+
+
+# cv.imshow('img', img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
