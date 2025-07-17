004819daa311904d5fd8f732a1a4f26740bae0c0
Adam Parsons <adam.a.parsons15@gmail.com>
2025-07-17 14:43:54 +0100

    Update arms_test.py
    arm moves very abruptly but smothly turns off

--- tests/arms_test.py
+++ tests/arms_test.py
@@ -1,3 +1,16 @@
 from reachy_sdk import ReachySDK
+import time
 
-reachy_mobile = ReachySDK(host='192.168.100.100')
+reachy = ReachySDK(host='192.168.100.100')
+time.sleep(3)  # wait for motion to complete
+
+reachy.turn_on('l_arm')
+print(reachy.joints.l_shoulder_pitch.present_position)
+
+# Set the target position in radians:
+reachy.joints.l_shoulder_pitch.goal_position = 20.0
+reachy.joints.l_elbow_pitch.goal_position = 20.0
+reachy.joints.l_shoulder_roll.goal_position = 20.0
+
+time.sleep(3)  # wait for motion to complete
+reachy.turn_off_smoothly('l_arm')
