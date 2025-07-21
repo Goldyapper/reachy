4c1304dc704f38e363e2d7f0f7700b400f1f2b29
Goldyapper <adam.a.parsons15@gmail.com>
2025-07-21 14:15:08 +0100

    made the turnaround into a function

--- tests/waypoint tests/go to one waypoint.py
+++ tests/waypoint tests/go to one waypoint.py
@@ -2,21 +2,24 @@ from reachy_sdk import ReachySDK
 import time
 import math
 
+def turnarround():
+    for i in range(6):
+        angle_deg = (i + 1) * 60
+        print(f"Rotating to {angle_deg}°...")
+        reachy.mobile_base.goto(x=0.0, y=0.0, theta=angle_deg)
+        time.sleep(1.5)
+
 reachy = ReachySDK(host='172.16.42.113', with_mobile_base=True)
 
 reachy.mobile_base.reset_odometry()
 reachy.mobile_base.drive_mode = 'brake'
 
 # Move forward diagonally
-reachy.mobile_base.goto(x=0.5, y=1.0, theta=0.0)
-time.sleep(3)
+#reachy.mobile_base.goto(x=0.5, y=1.0, theta=0.0)
+#time.sleep(3)
 
 # Rotate 360° in 60° steps
-for i in range(6):
-    angle_deg = (i + 1) * 60
-    print(f"Rotating to {angle_deg}°...")
-    reachy.mobile_base.goto(x=0.0, y=0.0, theta=angle_deg)
-    time.sleep(1.5)
+turnarround()
 
 # Return to origin
 reachy.mobile_base.goto(x=0.0, y=0.0, theta=0.0)
