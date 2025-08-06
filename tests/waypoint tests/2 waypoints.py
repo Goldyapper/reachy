b9bc2e0a4fea80364973eb90a48563bd303b256d
Goldyapper <adam.a.parsons15@gmail.com>
2025-08-06 12:29:20 +0100

    fixed waypoint 1 test so it works properly, need to work on staging steps

--- tests/waypoint tests/2 waypoints.py
+++ tests/waypoint tests/2 waypoints.py
@@ -7,21 +7,21 @@ reachy.mobile_base.reset_odometry()
 reachy.mobile_base.drive_mode = 'brake'
 
 # 1. Move forward 2 meters
-safe_goto(2.0, 0.0, 0.0)
+safe_goto(reachy,2.0, 0.0, 0.0)
 
 # 2. Turnaround (360 degrees in 60° steps)
-turnarround()
+turnarround(reachy)
 
 # 3. Move 1 meter left (positive Y axis)
-safe_goto(0.0, 1.0, 0.0)
+safe_goto(reachy,0.0, 1.0, 0.0)
 
 # 4. Turnaround again
-turnarround()
+turnarround(reachy)
 
 # 5. Return to origin: first go back 1 meter right (Y=0)
-safe_goto(0.0, -1.0, 0.0)
+safe_goto(reachy,0.0, -1.0, 0.0)
 # then go back 2 meters backward (X=0)
-move_backward_simulated(2.0, 0.0)
+move_backward_simulated(reachy,2.0, 0.0)
 # Final stop
 reachy.mobile_base.set_speed(x_vel=0.0, y_vel=0.0, rot_vel=0.0)
 reachy.mobile_base.drive_mode = 'brake'
